ebb05fc4335d42a2cace06c584e6190991507921
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-06-02 13:08:23 -0600

    Code working, but infinitel loop when adding even to odd

--- make-divisble.js
+++ make-divisble.js
@@ -1,11 +1,27 @@
 // make-divisble.js
 // Write a program that prompts the user for two numberx, x and y. The program should add one to x until it is divisble by y.
+const prompt = require('prompt-sync')();
 
-// Examples
-// Enter x: 
-// > 9
-// Enter y: 
-// > 1
+console.log('We will be taking two numbers. This program will add the second to the first repeatedly until that number is divisble by 4')
+let x = prompt('First number please.   ');
+let numX = Number(x);
+let y = prompt('Second number please.   ');
+let numY = Number(y);
+console.log('Your numbers are ', numX, '  and  ', numY);
+console.log('Together they equal ', numX + numY);
+numX = numX + numY;
+while (numX % 4 !== 0) {
+    numX = numX + numY;
+
+    console.log(numX);
+}
+console.log('');
+console.log(numX, ' is divisble by 4')
+    // Examples
+    // Enter x: 
+    // > 9
+    // Enter y: 
+    // > 1
 
 // 9 is divisible by 1
 // ```
