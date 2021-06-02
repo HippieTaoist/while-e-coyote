41e60acca87ceda1f46ae48794b8cca5a1bff204
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-06-02 12:03:46 -0600

    Initial Code Added

---
+++ number-doubler.js
@@ -0,0 +1,11 @@
+// number-doubler.js
+// Write a program that prompts the user for a number, then repeatedly doubles that number until it is greater than or equal to 100.
+
+// Example
+// Enter a number:
+// > 23
+
+// 23
+// 46
+// 92
+// 184
