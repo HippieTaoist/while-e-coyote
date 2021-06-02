867986a51d3f3f77c822fef9dd0206883a8a444b
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-06-02 12:11:27 -0600

    Ran Code | Works '

--- number-doubler.js
+++ number-doubler.js
@@ -9,3 +9,16 @@
 // 46
 // 92
 // 184
+
+const prompt = require('prompt-sync')();
+
+
+let initNum = prompt('Give me a number less than 180 double it up to and stop when close to 180');
+let numInitNum = Number(initNum);
+
+while (numInitNum < 180) {
+    console.log(numInitNum);
+    numInitNum = numInitNum * 2;
+}
+
+console.log("That's All Folks")
